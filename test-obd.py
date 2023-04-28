4605171ba1dd70c3fee4c8996095c16602680a32
Firmin Launay <hey@firminlaunay.me>
2023-04-28 22:34:53 +0200

    Add code for OBD logging

--- test-obd.py
+++ test-obd.py
@@ -1,10 +1,18 @@
 from obd import OBD, commands
-from time import sleep
+from time import sleep, time
 
 connection = OBD()  # connect to OBD adapter
 
 while True:
-    cmd = commands.SPEED  # define command as SPEED
-    response = connection.query(cmd)  # send the command
-    print(response.value)  # print result
+    cmd = commands.SPEED	  # define command as SPEED
+    speed = connection.query(cmd)  # send the command
+    print(speed)
+    cmd = commands.RPM
+    rpm = connection.query(cmd)
+    print(rpm)
+    cmd = commands.INTAKE_TEMP
+    intake_temperature = connection.query(cmd)
+    print(intake_temperature)
+    with open("output.csv", 'a') as f:
+        f.write(str(time()) + ';' + str(speed) + ';' + str(rpm) + ';' + str(intake_temperature) + '\n')
     sleep(1)
