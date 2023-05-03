2dc387380dbf493eb1c5eaf61fa35d20b48f6a51
Lxmonexe <93253891+Lxmonexe@users.noreply.github.com>
2023-05-03 11:12:53 +0200

    fix csv file creation for travel

--- retrieve_data.py
+++ retrieve_data.py
@@ -1,8 +1,11 @@
 from obd import OBD, commands
 from time import sleep, time
+from datetime import datetime
 
 connection = OBD()  # connect to OBD adapter
-
+travel_name = "travel_" + datetime.today().strftime('%Y%m%d_%H%M%S') + ".csv"
+with open(travel_name, 'a') as f:   # create csv file to log data from the travel
+    f.write('time' + ';' + 'speed_kph' + ';' + 'rpm' + ';' + 'intake_temperature' + '\n')
 while True:
     cmd = commands.SPEED	  # define command as SPEED
     speed = connection.query(cmd)  # send the command
@@ -13,6 +16,7 @@ while True:
     cmd = commands.INTAKE_TEMP
     intake_temperature = connection.query(cmd)
     print(intake_temperature)
-    with open("output.csv", 'a') as f:
-        f.write(str(time()) + ';' + str(speed) + ';' + str(rpm) + ';' + str(intake_temperature) + '\n')
+    with open(travel_name, 'a') as f:
+        f.write(str(time()) + ';' + str(speed.value.value) + ';' + str(rpm.value.value) + ';'
+                + str(intake_temperature.value.value) + '\n')
     sleep(1)
