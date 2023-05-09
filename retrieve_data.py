ca98976374d7ff0bcd60d69ede3c6166c7746a66
Firmin Launay <hey@firminlaunay.me>
2023-05-09 15:33:10 +0200

    Minor fixes

--- retrieve_data.py
+++ retrieve_data.py
@@ -7,7 +7,7 @@ from threading import Thread
 connection = OBD()  # connect to OBD adapter
 travel_name = "travel_" + datetime.today().strftime('%Y%m%d_%H%M%S') + ".csv"
 with open(travel_name, 'a') as f:  # create csv file to log data from the travel
-    f.write('time' + ';' + 'speed_kph' + ';' + 'rpm' + ';' + 'intake_temperature_°C' + '\n')
+    f.write('time' + ';' + 'speed_kph' + ';' + 'rpm' + ';' + 'intake_temperature_degC' + '\n')
 
 app = Flask(__name__)
 
@@ -27,17 +27,17 @@ def pull_data():
     while True:
         cmd = commands.SPEED  # define command as SPEED
         speed = connection.query(cmd)  # send the command
-        DATA["speed"] = speed.value.value
+        DATA["speed"] = speed.value.m
         cmd = commands.RPM
         rpm = connection.query(cmd)
-        DATA["rpm"] = rpm.value.value
+        DATA["rpm"] = rpm.value.m
         cmd = commands.INTAKE_TEMP
         intake_temperature = connection.query(cmd)
-        DATA["intake_temp"] = intake_temperature.value.value
+        DATA["intake_temp"] = intake_temperature.value.m
         with open(travel_name, 'a') as ft:
-            ft.write(str(time()) + ';' + str(speed.value.value) + ';' + str(rpm.value.value) + ';'
-                     + str(intake_temperature.value.value) + '\n')
-        sleep(0.1)
+            ft.write(str(time()) + ';' + str(speed.value.m) + ';' + str(rpm.value.m) + ';'
+                     + str(intake_temperature.value.m) + '\n')
+        print(str(time()) + " : new record")
 
 
 Thread(target=pull_data).start()
