b361c4d62f2814c3bad9d0666da70cb19017bb86
Lxmonexe <93253891+Lxmonexe@users.noreply.github.com>
2023-06-01 16:05:36 +0200

    Implement auto travel detection

--- retrieve_data.py
+++ retrieve_data.py
@@ -8,9 +8,11 @@ from werkzeug.exceptions import NotFound
 import flask_app
 
 connection = OBD()  # connect to OBD adapter
-travel_name = "travel_" + datetime.today().strftime('%Y%m%d_%H%M%S') + ".csv"
-with open(travel_name, 'a') as f:  # create csv file to log data from the travel
-    f.write('time' + ';' + 'speed_kph' + ';' + 'rpm' + ';' + 'intake_temperature_degC' + '\n')
+
+
+def car_on(rpm: int) -> bool:
+    return rpm != 0
+
 
 DATA = {
     "speed": 0,
@@ -34,19 +36,26 @@ def get_data() -> Response:
 
 def pull_data() -> None:
     while True:
-        cmd = commands.SPEED  # define command as SPEED
-        speed = connection.query(cmd)  # send the command
-        DATA["speed"] = speed.value.m
         cmd = commands.RPM
         rpm = connection.query(cmd)
-        DATA["rpm"] = rpm.value.m
-        cmd = commands.INTAKE_TEMP
-        intake_temperature = connection.query(cmd)
-        DATA["intake_temp"] = intake_temperature.value.m
-        with open(travel_name, 'a') as ft:
-            ft.write(str(time()) + ';' + str(speed.value.m) + ';' + str(rpm.value.m) + ';'
-                     + str(intake_temperature.value.m) + '\n')
-        print(str(time()) + " : new record")
+        if car_on(rpm):
+            travel_name = "travel_" + datetime.today().strftime('%Y%m%d_%H%M%S') + ".csv"
+            with open(travel_name, 'a') as f:  # create csv file to log data from the travel
+                f.write('time' + ';' + 'speed_kph' + ';' + 'rpm' + ';' + 'intake_temperature_degC' + '\n')
+            while car_on(rpm):
+                cmd = commands.RPM
+                rpm = connection.query(cmd)
+                DATA["rpm"] = rpm.value.m
+                cmd = commands.SPEED  # define command as SPEED
+                speed = connection.query(cmd)  # send the command
+                DATA["speed"] = speed.value.m
+                cmd = commands.INTAKE_TEMP
+                intake_temperature = connection.query(cmd)
+                DATA["intake_temp"] = intake_temperature.value.m
+                with open(travel_name, 'a') as ft:
+                    ft.write(str(time()) + ';' + str(speed.value.m) + ';' + str(rpm.value.m) + ';'
+                             + str(intake_temperature.value.m) + '\n')
+                print(str(time()) + " : new record")
 
 
 Thread(target=pull_data).start()
